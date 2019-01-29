cd32159646d41a1eafedd7a6d96ed1b0be04325d
Salem Yaslem <xlmnxp@outlook.sa>
2019-01-29 23:48:37 +0300

    add support to ios

--- absolute-layout/absolute-layout.ios.ts
+++ absolute-layout/absolute-layout.ios.ts
@@ -1,5 +1,63 @@
-import { Common } from './absolute-layout.common';
+import { Common, isRtlProperty } from "./absolute-layout.common";
+import { View } from "tns-core-modules/ui/core/view/view";
 
 export class AbsoluteLayout extends Common {
+  public initNativeView(): void {
+    super.initNativeView();
+    this._updateDirection();
+  }
 
+  [isRtlProperty.setNative](isRtl: boolean): void {
+    this.isRtl = isRtl;
+    this._updateDirection();
+  }
+
+  public addChild(view: View): void {
+    super.addChild(view);
+    if (view.nativeViewProtected) {
+      this._updateDirection();
+    }
+  }
+
+  public removeChild(view: View): void {
+    super.removeChild(view);
+    if (view.nativeViewProtected) {
+      this._updateDirection();
+    }
+  }
+
+  private _updateDirection(): void {
+    setTimeout(() => {
+      if (this.isRtl) {
+        this.nativeViewProtected.transform = CGAffineTransformMakeRotation(
+          (180.0 * Math.PI) / 180.0
+        );
+        for (
+          let viewIndex = 0;
+          viewIndex < this["getChildrenCount"]();
+          viewIndex++
+        ) {
+          let NSView: View = this["getChildAt"](viewIndex);
+          let isRtl: boolean = NSView["isRtl"] || false;
+          if (isRtl) {
+            NSView.nativeView.transform = CGAffineTransformMakeRotation(0);
+          } else {
+            NSView.nativeView.transform = CGAffineTransformMakeRotation(
+              (180.0 * Math.PI) / 180.0
+            );
+          }
+        }
+      } else {
+        this.nativeViewProtected.setRotationY(0);
+        for (
+          let viewIndex = 0;
+          viewIndex < this["getChildrenCount"]();
+          viewIndex++
+        ) {
+          let NSView: View = this["getChildAt"](viewIndex);
+          NSView.nativeView.transform = CGAffineTransformMakeRotation(0);
+        }
+      }
+    }, 1);
+  }
 }
